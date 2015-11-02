6105af5f3e71a47148c8afbb689e5e44fe572945
Badim150 <cm_margarido@hotmail.com>
2015-11-02 14:06:35 +0000

    troll cons

---
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/Pipeline/CarsAndTrolls/TrollConstraint.cs
@@ -0,0 +1,60 @@
+﻿using Assets.Scripts.IAJ.Unity.Movement.DynamicMovement.Pipeline.Interfaces;
+using Assets.Scripts.IAJ.Unity.Pathfinding.Path;
+using Assets.Scripts.IAJ.Unity.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement.Pipeline
+{
+    class TrollConstraint : Constraint
+    {
+        public float margin { set; get; }
+        public float radius { set; get; } = 0.2f;
+      //  public Vector3 center { set; get; }
+        public int probInd { set; get; }
+        DynamicCharacter troll { set; get; }
+
+
+        public Boolean WillViolate(GlobalPath path)
+        {
+            int ind = 0;
+            foreach (LocalPath p in path.LocalPaths)
+            {              
+                if (MathHelper.closestParamInLineSegmentToPoint(p.StartPosition, p.EndPosition, troll.KinematicData.position) < radius){
+                    probInd = ind;
+                    return true;
+                }
+                ind++;
+            }
+            return false;
+        }
+
+
+        public Goal Suggest(LineSegmentPath path, KinematicData character, Goal goal)
+        {
+            // procurar ponto do segmento mais próximo ao centro da esfera
+            Vector3 closest = path.GetPosition(MathHelper.closestParamInLineSegmentToPoint(path.StartPosition, path.EndPosition, troll.KinematicData.position));
+            // Check if we pass through the center point
+            Vector3 newPt;
+            if (closest.sqrMagnitude == 0)
+            {
+                // Get any vector at right angles to the segment
+                Vector3 dirn =  path.EndPosition - path.StartPosition;
+                //pode nao ser esta func TO DO
+                Vector3 newdirn = Vector3.Cross(dirn, Vector3.Cross(dirn, Vector3.right));
+                newPt = troll.KinematicData.position + newdirn * radius * margin;
+            }
+            else
+            {
+                // Otherwise project the point out beyond the radius
+                newPt = troll.KinematicData.position + (closest - troll.KinematicData.position) * radius * margin / closest.sqrMagnitude;
+            }
+            // Set up the goal and return
+            goal.position = newPt;
+            return goal;
+        }
+    }
+}
